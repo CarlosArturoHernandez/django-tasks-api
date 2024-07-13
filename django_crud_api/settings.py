4dec91439e886e1e936e28d47aaac2edd4b273e4
Carlos A. <hcarlos780@gmail.com>
2024-07-13 16:03:24 -0600

    Add allowed hots to settings.py

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-rb(f88mw9lw)%xt%z%cw^#!y+d$o#mm98ow42ii8e2el4th$n-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    'django-tasks-api-production.up.railway.app'
+]
 
 
 # Application definition
